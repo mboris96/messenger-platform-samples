bdf838092e557d01ddb13f39dad04d4d0a0ea57e
Matt Boris <mboris@iruleathome.com>
2017-07-27 12:58:56 -0400

    Add postback response

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ app.post('/webhook', function (req, res) {
         if (event.message) {
             receivedMessage(event);
         }
+        else if (event.postback) {
+            receivedPostback(event);
+        }
         else {
             console.log("Webhook received unknown event: ", event);
         }
@@ -158,6 +161,23 @@ function sendGenericMessage(recipientId) {
     };  
 
     callSendAPI(messageData);
+}
+
+function receivedPostback(event) {
+    var senderID = event.sender.id;
+    var recipientID = event.recipient.id;
+    var timeOfPostback = event.timestamp;
+
+    // The 'payload' param is a developer-defined field which is set in a postback
+    // button for Structured Messages.
+    var payload = event.postback.payload;
+
+    console.log("Received postback for user %d and page %d with payload '%s' " +
+        "at %d", senderID, recipientID, payload, timeOfPostback);
+
+    // When a postback is called, we'll send a message back to the sender to
+    // let them know it was successful
+    sendTextMessage(senderID, "Postback called");
 }
 
 function callSendAPI(messageData) {
